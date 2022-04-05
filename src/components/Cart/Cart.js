67ee15a9c01ae42364c5f4e9c01b376ab3aded18
ajoy biswas <ajoypust@gmail.comm>
2022-04-05 12:57:43 +0600

    order summery function add

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import'./Cart.css'
 const Cart = ({cart}) => {
-     console.log(cart)
+    //  console.log(cart);
+    let total=0;
+    let shipping=0;
+    let tax=0;
+    let grandTotal=0;
+     for(const product of cart){
+        //  console.log(product);
+         total=total+product.price;
+         shipping=shipping+product.shipping;
+          tax=total*5/100;
+          grandTotal=total+shipping+tax;
+     }
     return (
-        <div>
+        <div className='cart'>
             <h1>Order summery</h1>
-             <p>Selected Item:{cart.length}</p> 
+             <p>Selected Item:{cart.length}</p>
+             <p>total price:${total}</p> 
+             <p>total shipping:${shipping}</p>
+             <p>tax:${tax}</p>
+             <h3>GRAND TOTAL:${grandTotal}</h3>
         </div>
     );
 };
